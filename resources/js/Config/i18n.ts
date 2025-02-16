4d4d3ed2c78c40e281ecbb039e40c0666b1b9727
Diandreas <127276266+Diandreas@users.noreply.github.com>
2025-02-16 04:49:59 +0100

    langue dans le cache

--- resources/js/Config/i18n.ts
+++ resources/js/Config/i18n.ts
@@ -1,6 +1,6 @@
-// resources/js/config/i18n.ts
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import en from '../locales/en.json';
 import fr from '../locales/fr.json';
 
@@ -9,12 +9,20 @@ const resources = {
     fr: { translation: fr }
 };
 
+
 i18n
+    .use(LanguageDetector)  // Ajoute la détection de langue
     .use(initReactI18next)
+    // @ts-ignore
     .init({
         resources,
-        lng: "fr",
         fallbackLng: "en",
+        detection: {
+            order: ['localStorage', 'navigator'],
+            lookupLocalStorage: 'i18nextLng',
+            caches: ['localStorage'],
+            checkWhitelist: true
+        },
         interpolation: {
             escapeValue: false
         },

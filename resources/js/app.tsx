9197ba65d2396e788d077cd82edfd67f59277f0a
Diandreas <127276266+Diandreas@users.noreply.github.com>
2025-03-15 14:06:45 +0100

    lnagues ok

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,7 +4,7 @@ import './Config/i18n';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import {ThemeProvider} from "next-themes";
+import { ThemeProvider } from "next-themes";
 
 const appName = import.meta.env.VITE_APP_NAME || 'GUIDY';
 
